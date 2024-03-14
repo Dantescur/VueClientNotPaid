31ef9ac8951be9dba3b0eba8112e1ad0911c5b0f
NAME <mail@gmail.com>
2024-03-13 21:44:02 -0400

    A  package-lock.json M  package.json M  pnpm-lock.yaml M  src/index.ts M  vite.config.ts

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,26 @@ interface NotPaidOptions {
   deadline: number;
 }
 
+const calculateOpacity = (options: NotPaidOptions): number => {
+  const currentDate = new Date();
+  const dueDate = new Date(options.dueDate);
+  const deadlineDate = new Date(
+    dueDate.getTime() + options.deadline * 24 * 60 * 60 * 1000,
+  );
+
+  if (currentDate > deadlineDate) {
+    return 0;
+  } else if (currentDate > dueDate) {
+    const dayPastDue =
+      (currentDate.getTime() - dueDate.getTime()) / (1000 * 60 * 60 * 24);
+    return (options.deadline - dayPastDue) / options.deadline;
+  }
+  return 1;
+};
+
 const VueClientNotPaid = {
   install(app: App, options: NotPaidOptions): void {
-    const calculateOpacity = (): number => {
-      const currentDate = new Date();
-      const dueDate = new Date(options.dueDate);
-      const deadlineDate = new Date(
-        dueDate.getTime() + options.deadline * 24 * 60 * 60 * 1000,
-      );
-
-      if (currentDate > deadlineDate) {
-        return 0;
-      } else if (currentDate > dueDate) {
-        const dayPastDue =
-          (currentDate.getTime() - dueDate.getTime()) / (1000 * 60 * 60 * 24);
-        return (options.deadline - dayPastDue) / options.deadline;
-      }
-      return 1;
-    };
-    app.mixin({
-      mounted() {
-        this.$nextTick(() => {
-          document.body.style.opacity = calculateOpacity().toString();
-        });
-      },
-    });
+    app.config.globalProperties.$opacity = calculateOpacity(options);
   },
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
@@ -6,15 +7,16 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: "src/index.ts",
+      entry: path.resolve(__dirname, "src/index.ts"),
       name: "VueClientNotPaid",
+      formats: ["es"],
       fileName: "vue-client-notpaid",
     },
     rollupOptions: {
       external: ["vue"],
       output: {
         globals: {
-          vue: "Vue",
+          Vue: "vue",
         },
       },
     },
